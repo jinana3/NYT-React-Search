d3d342b6602be7141db30b2705e69388d0417e08
Jina Li <jinali.berkeley@gmail.com>
2018-05-05 09:22:37 -0500

    Update articleController.js

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -14,17 +14,6 @@ module.exports = {
       .then(dbArticle => res.json(dbArticle))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
-    const article = {
-      _id: req.body._id,
-      title: req.body.headline.main,
-      url: req.body.web_url
-    };
-    db.Article
-      .create(article)
-      .then(dbArticle => res.json(dbArticle))
-      .catch(err => res.status(422).json(err));
-  },
   update: function(req, res) {
     db.Article
       .findOneAndUpdate({ _id: req.params.id }, req.body)
